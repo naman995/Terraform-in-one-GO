94fb702bc6678a06a2922b69a251f3416b1220b4
naman995 <rajputnaman823@gmail.com>
2025-05-21 10:35:42 +0530

    loops

--- terraform-for-devops/output.tf
+++ terraform-for-devops/output.tf
@@ -15,3 +15,13 @@ output "ec2_private_ip" {
     for instance in aws_instance.my_instance : instance.private_ip
   ]
 }
+output "ec2_public_ip" {
+  value = [
+    for instance in aws_instance.my_instance : instance.public_ip
+  ]
+}
+output "ec2_public_dns" {
+  value = [
+    for instance in aws_instance.my_instance : instance.public_dns
+  ]
+}
